5ca35676a4cda0a800db48ea35e2f55ca4f690e4
B-loeb <bradleyloeb@gmail.com>
2022-12-13 19:07:39 -0500

    User Schema made

---
+++ api/User.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+//Signup
+router.post('/signup', (req, res) => {
+
+})
+
+//Login
+router.post('/login', (req, res) => {
+
+})
+module.exports = router;
